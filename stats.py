07124ba6495761c0c3cbfc20ede60d2455759d87
mstromberg3 <157034943+mstromberg3@users.noreply.github.com>
2024-03-07 23:41:09 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,43 +1,33 @@
+"""
+this document is for the stats page and includes all functions and classes necessary to run that page (or they are imported)
+"""
+from classes import *
 
-# now creating the stats window (because I don't know how to do it in a separate doc)
-from os import fsdecode
-import tkinter as tk
-from tkinter import HORIZONTAL, LEFT, TOP, font, messagebox 
-from tkinter.ttk import *
-
-window2 = tk.Toplevel()
-
-# random color generator (for testing purposes)
-import random
-
-def random_color_generator():
-    r = random.randint(0, 999999)
-    return r
+window2 = tk.Toplevel(window)
 
 # frame config
 frame2 = tk.Frame(window2, bg="#4a4a4a", padx=10, pady=10)
-frame2.grid()
-frame2.pack(anchor='center')
+frame2.grid(row=0, column=0)
+# frame2.pack(anchor='center')
 
-def window2Config():
+def windowsConfigs(windowName):
   # window configuration
-  window2.title("  RPG Character Builder")
-  window2.geometry("400x400")
-  window2.configure(bg='#4a4a4a', padx=10, pady=10)
+  windowName.title("  RPG Character Builder")
+  windowName.geometry("400x420")
+  windowName.configure(bg='#4a4a4a', padx=10, pady=10)
+  icon = tk.PhotoImage(file = "icon.png")
+  windowName.iconphoto(False, icon)
 
-
-# set icon image
-icon = tk.PhotoImage(file = "icon.png")
-window2.iconphoto(False, icon)
+def backClassCmd():
+  # brings back main window
+  window.deiconify()
+  classy.set_class("Hello")
+  from skills import determine_Skillz
+  determine_Skillz()
 
 # back arrow
 backArrow = tk.PhotoImage(file="back.png")
-backToClass = tk.Button(frame2, image=backArrow, width=30, height=15, borderwidth=0, bg='#4a4a4a', command=lambda:[window2.withdraw()])
-
-# header label
-headFrame = tk.Frame(frame2, padx=5, pady=5, bg='#4a4a4a')
-headFrame.grid(row=1, column=0, columnspan=2)
-headLabel = tk.Label(headFrame, text="CHOOSE YOUR STATS", bg='white', fg='black', font=font.Font(size=14))
+backToClass = tk.Button(frame2, image=backArrow, width=30, height=15, borderwidth=0, bg='#4a4a4a', command=backClassCmd)
 
 # create frame for explanation
 explainFrame = tk.Frame(frame2, padx=10, pady=10)
@@ -45,19 +35,20 @@ explainFrame.grid(row=2, column=0, rowspan=3)
 
 # Explanation class (this took FOREVER)
 class Explanation:
-  def __init__(self,label,row,column):
+  def __init__(self,frameName,label,row,column):
     self.l = label
     self.x = row
     self.y = column
+    self.f = frameName
   def label(self):
-    tk.Label(explainFrame, text=self.l, font=font.Font(size=9),wraplength=150, height=3).grid(row=self.x, column=self.y)
+    tk.Label(self.f, text=self.l, font=font.Font(size=9),wraplength=150, height=3).grid(row=self.x, column=self.y)
 
 # text within Explanation
-line1_exp = Explanation("Choose a number for each stat using the slider.",1,0)
-line2_exp = Explanation("All three numbers must add up to exactly 100 to move to next page.",2,0)
-line3_exp = Explanation("Click \"Get Total\" to calculate current total.",3,0)
-line4_exp = Explanation("Stats have been set to the default for your class.",4,0)
-line5_exp = Explanation("You can set the stats to near-even numbers with the Reset button.",5,0)
+line1_exp = Explanation(explainFrame,"Choose a number for each stat using the slider.",1,0)
+line2_exp = Explanation(explainFrame, "All three numbers must add up to exactly 100 to move to next page.",2,0)
+line3_exp = Explanation(explainFrame, "Click \"Get Total\" to calculate current total.",3,0)
+line4_exp = Explanation(explainFrame, "Stats have been set to the default for your class.",4,0)
+line5_exp = Explanation(explainFrame, "You can set the stats to near-even numbers with the Reset button.",5,0)
 
 def explainLines():
   # function to display Explanation text
@@ -67,11 +58,10 @@ def explainLines():
   line4_exp.label()
   line5_exp.label()
 
-
 # Config for each stat frame
 class Stats:
   def __init__(self, slideValName, row, column):
-    #slideValName is the skill name, row and column are for grid placement
+    #slideValName is the stat name, row and column are for grid placement
     self.name = slideValName
     self.x = row
     self.y = column
@@ -82,9 +72,18 @@ class Stats:
     statFrames.grid(row=self.x, column=self.y)
     # v1 = tk.DoubleVar() # figure out how to make this a text entry too
     # label specs
-    l1 = tk.Label(statFrames, text=self.name, bg='#4a4a4a', fg='white').grid(row=0, column=0, sticky='s')
+    l1 = tk.Label(statFrames, text=self.name, font=font.Font(size=10, weight='bold'), bg='#4a4a4a', fg='white').grid(row=0, column=0, sticky='s')
     # slider specs
-    s1 = tk.Scale(statFrames, variable=self.v1, from_=1, to=100, orient=HORIZONTAL, sliderlength=17).grid(row=1, column=0, padx=20, pady=5)
+    s1 = tk.Scale(statFrames, variable=self.v1, from_=1, to=100, orient=HORIZONTAL, length=150, sliderlength=17).grid(row=1, column=0, padx=5, pady=5)
+    """ # can't get this section to work
+    # entry box specs
+    enterVal = tk.Entry(statFrames, width=4)
+    enterVal.grid(row=2, pady=5)
+    def setVal(self):
+      enterValFloat = float(enterVal.get())
+      self.v1.set(enterValFloat)
+    enterVal.bind('<Return>', 'setVal')
+    """
 
 
 # define different stats
@@ -92,7 +91,7 @@ survivalSlider = Stats("Survival Instincts",2,1)
 empathySlider = Stats("Empathy",3,1)
 driveSlider = Stats("Drive",4,1)
 
-# this next series of functions calclulates and displays total value of all sliders
+# these next two functions calclulate and display total value of all sliders
 
 def get_slider_vals():
   # get the value of each slider
@@ -124,11 +123,26 @@ valButton = tk.Button(buttonsFrame, text="Get Total", command=show_total)
 showVal = tk.Label(buttonsFrame, bg='#4a4a4a')
 
 # reset button
-def re_reset(s=33,e=34,d=33):
-  # sets stats back to default
-  survivalSlider.v1.set(s) # placeholder numbers. Will update with classes
-  empathySlider.v1.set(e)
-  driveSlider.v1.set(d)
+def re_reset():  
+  # sets stats to default for each class
+  if classy.get_class()=="mobster":
+    survivalSlider.v1.set(10)
+    empathySlider.v1.set(30)
+    driveSlider.v1.set(60)
+  else:
+    if classy.get_class()=="agent":
+      survivalSlider.v1.set(20)
+      empathySlider.v1.set(10)
+      driveSlider.v1.set(70)
+    else:
+      if classy.get_class()=="teetotal":
+        survivalSlider.v1.set(5)
+        empathySlider.v1.set(47)
+        driveSlider.v1.set(48)
+      else:
+        survivalSlider.v1.set(33)
+        empathySlider.v1.set(34)
+        driveSlider.v1.set(33)    
   show_total()
 
 re_resetButton = tk.Button(buttonsFrame, text="Reset", command=re_reset)
@@ -137,9 +151,10 @@ re_resetButton = tk.Button(buttonsFrame, text="Reset", command=re_reset)
 def to_skills():
   # if total is 100, moves to next page. If not, displays error
   if get_slider_vals() == 100: # still need to edit this to move to next page. Currently test placeholder
-    buttonsFrame.config(bg="#"+str(random_color_generator()))
+    window2.withdraw()
+    # buttonsFrame.config(bg="#"+str(random_color_generator()))
   else:
-    messagebox.showerror('Program Error', 'Error: cannot move to next page until total equals 100')
+    messagebox.showerror('Program Error', 'Error: cannot move to next page until total equals 100', parent=window2)
 
 nextButton = tk.Button(buttonsFrame, text="Next", fg='#9a9a9a', command=to_skills)
 
@@ -150,24 +165,11 @@ def bottomButtons():
   re_resetButton.grid(row=0, column=2, padx=10)
   nextButton.grid(row=0, column=3, padx=10, sticky='e')
 
-"""
-def labelConfig(label,x,y):
-  # label configuration
-  label_font = font.Font(size=12)
-  label = tk.Label(frame2, text=label, bg='#4a4a4a', fg='white', font=label_font, wraplength=100, height=2)
-  label.grid(row=x, column=y)
-
-def buttonConfig(words,pic,x,y):
-  # create and style button
-  button = tk.Button(frame2, text=words, image=pic, width=100, height=100)
-  button.grid(row=x,column=y, padx=10, pady=10)
-  """
-
 # run all functions
 def statsPage():
-  window2Config()
+  windowsConfigs(window2)
   backToClass.grid(row=0, column=0, ipadx=0, ipady=0, padx=10, sticky='w')
-  headLabel.grid(row=0, column=0, ipadx=5, ipady=5, padx=10)
+  headerLabel(frame2, "CHOOSE YOUR STATS", 2)
   explainLines()
   survivalSlider.showit()
   empathySlider.showit()
